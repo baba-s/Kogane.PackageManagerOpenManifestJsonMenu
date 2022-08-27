1c65cd97ed687a7e9b7bfeb2b9d7afa6c738912f
baba-s <shotababa1206@gmail.com>
2022-08-27 14:37:43 +0900

    add

---
+++ Editor/PackageManagerOpenManifestJsonMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEditor.PackageManager.UI.Internal;
+using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace Kogane.Internal
+{
+    internal sealed class PackageManagerOpenManifestJsonMenu
+        : VisualElement,
+          IPackageManagerExtension
+    {
+        private bool m_isInitialized;
+
+        VisualElement IPackageManagerExtension.CreateExtensionUI()
+        {
+            m_isInitialized = false;
+            return this;
+        }
+
+        void IPackageManagerExtension.OnPackageSelectionChange( PackageInfo packageInfo )
+        {
+            if ( m_isInitialized ) return;
+
+            VisualElement root = this;
+
+            while ( root != null && root.parent != null )
+            {
+                root = root.parent;
+            }
+
+            var templateContainer     = root.Q<TemplateContainer>();
+            var packageManagerToolbar = templateContainer.Q<PackageManagerToolbar>();
+            var menuDropdownItem      = packageManagerToolbar.toolbarSettingsMenu.AddBuiltInDropdownItem();
+
+            menuDropdownItem.text = "Open manifest.json";
+            menuDropdownItem.action = () =>
+            {
+                var fullPath = Path.GetFullPath( "./Packages/manifest.json" );
+                Process.Start( fullPath );
+            };
+
+            m_isInitialized = true;
+        }
+
+        void IPackageManagerExtension.OnPackageAddedOrUpdated( PackageInfo packageInfo )
+        {
+        }
+
+        void IPackageManagerExtension.OnPackageRemoved( PackageInfo packageInfo )
+        {
+        }
+
+        [InitializeOnLoadMethod]
+        private static void InitializeOnLoadMethod()
+        {
+            var extension = new PackageManagerOpenManifestJsonMenu();
+            PackageManagerExtensions.RegisterExtension( extension );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.PackageManagerOpenManifestJsonMenu
+# Kogane Package Manager Open Manifest Json Menu
+
+Package Manager に manifest.json を開くメニューを追加するエディタ拡張
